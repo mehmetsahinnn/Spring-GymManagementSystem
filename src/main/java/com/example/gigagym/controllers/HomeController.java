cff4b2c126034f4f0bd894ecb75cf3b4b3134a28
Mehmet <mehmetsahinn2002@gmail.com>
2023-05-27 02:36:07 +0300

    Added little security system via directing

--- src/main/java/com/example/gigagym/controllers/HomeController.java
+++ src/main/java/com/example/gigagym/controllers/HomeController.java
@@ -1,14 +1,11 @@
 package com.example.gigagym.controllers;
 
 
-import com.example.gigagym.services.EquipmentService;
-import com.example.gigagym.services.MaintenanceService;
-import com.example.gigagym.services.MemberService;
-import com.example.gigagym.services.PaymentService;
+import com.example.gigagym.services.*;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class HomeController {
@@ -16,25 +13,40 @@ public class HomeController {
     private final PaymentService paymentService;
     private final MaintenanceService maintenanceService;
     private final EquipmentService equipmentService;
+    private final StaffService staffService;
 
 
-    public HomeController(MemberService memberService, PaymentService paymentService, MaintenanceService maintenanceService, EquipmentService equipmentService) {
+    public HomeController(MemberService memberService, PaymentService paymentService, MaintenanceService maintenanceService, EquipmentService equipmentService, StaffService staffService) {
         this.memberService = memberService;
         this.paymentService = paymentService;
         this.maintenanceService = maintenanceService;
         this.equipmentService = equipmentService;
+        this.staffService = staffService;
     }
 
     @RequestMapping("/home")
-    public String index(Model model) {
+    public String index(Model model, HttpSession session) {
+        String StaffName = (String) session.getAttribute("StaffName");
+        String JobTitle = (String) session.getAttribute("JobTitle");
+
 
         model.addAttribute("numberOfUsersLastMonth", memberService.getNumberOfUsersRegisteredLastMonth());
         model.addAttribute("lastMonthEarnings", memberService.calculateLastMonthEarnings());
         model.addAttribute("totalCharges", memberService.calculateTotalCharges());
         model.addAttribute("totalPayment", paymentService.calculateTotalPaymentDividedByFive());
-        model.addAttribute("UpcomingMaintenance", maintenanceService.getUpcomingMaintenanceEquipmentCount());
         model.addAttribute("mostCommonEquipmentType", equipmentService.getMostCommonEquipmentType());
+        model.addAttribute("maleRatio", memberService.getMaleRatio() * 100);
+        model.addAttribute("femaleRatio", memberService.getFemaleRatio() * 100);
+        model.addAttribute("maintenanceCount", equipmentService.nextMaintenance());
+        model.addAttribute("StaffName", StaffName);
 
-        return "index";
+
+        if ("Maintenance Guy".equals(JobTitle)) {
+
+            return "redirect:/maintenance";
+        }
+        else {
+            return "index";
+        }
     }
 }
